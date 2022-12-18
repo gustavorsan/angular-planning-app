d45d95e0d0cf6a4d4d86ebd94d1bb50e2d6dd747
gustavorsan <gustavorsantos42@gmail.com>
2022-12-18 12:32:30 -0300

    Criado componente de header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { RecipeBookComponent } from './pages/recipe-book/recipe-book.component';
+import { RecipeListComponent } from './pages/recipe-book/recipe-list/recipe-list.component';
+import { RecipeDetailComponent } from './pages/recipe-book/recipe-detail/recipe-detail.component';
+import { RecipeItemComponent } from './pages/recipe-book/recipe-list/recipe-item/recipe-item.component';
+import { ShoppingListComponent } from './pages/shopping-list/shopping-list.component';
+import { ShoppingListEditComponent } from './pages/shopping-list/shopping-list-edit/shopping-list-edit.component';
+import { ShoppingListItemComponent } from './pages/shopping-list/shopping-list-item/shopping-list-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    RecipeBookComponent,
+    RecipeListComponent,
+    RecipeDetailComponent,
+    RecipeItemComponent,
+    ShoppingListComponent,
+    ShoppingListEditComponent,
+    ShoppingListItemComponent
   ],
   imports: [
     BrowserModule,
